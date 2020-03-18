ab8f5834e5531e5d7e0bd6d71286c58cb2d61296
Ahmet Durmic <ahmetdurmic97@outlook.com>
2020-03-18 17:08:20 +0100

    Configured mapping profiles

--- WebApp/WebApp/AutoMapper/MappingProfile.cs
+++ WebApp/WebApp/AutoMapper/MappingProfile.cs
@@ -3,10 +3,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApp.Dtos;
+using WebApp.Models;
 
 namespace WebApp.AutoMapper
 {
     public class MappingProfile : Profile
     {
+        public MappingProfile()
+        {
+            CreateMap<Employee, EmployeeDto>();
+            CreateMap<EmployeeDto, Employee>();
+
+            CreateMap<Department, DepartmentDto>();
+            CreateMap<DepartmentDto, Department>();
+        }
     }
 }
