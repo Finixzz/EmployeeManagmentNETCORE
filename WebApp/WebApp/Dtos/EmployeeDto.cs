1d6060e99684f58a0bcf03d0fd0245a0363ad8df
Ahmet Durmic <ahmetdurmic97@outlook.com>
2020-03-18 17:07:50 +0100

    Added dtos

---
+++ WebApp/WebApp/Dtos/EmployeeDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Dtos
+{
+    public class EmployeeDto
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "The email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required]
+        public int DepartmentId { get; set; }
+    }
+}
