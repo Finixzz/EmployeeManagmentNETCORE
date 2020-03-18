43fb0710442188b0524d6cd19e86c6aa4beb8d51
Ahmet Durmic <ahmetdurmic97@outlook.com>
2020-03-18 12:53:22 +0100

    Added employee and department models

---
+++ WebApp/WebApp/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "The email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required]
+        public int DepartmentId { get; set; }
+
+        public Department Department { get; set; }
+    }
+}
