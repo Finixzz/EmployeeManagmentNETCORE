b900481c04ffb8fe29a18010b4a2e1b1743c186b
Ahmet Durmic <ahmetdurmic97@outlook.com>
2020-03-18 14:14:05 +0100

    Added Interfaces

---
+++ WebApp/WebApp/Models/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models.Interfaces
+{
+    public interface IDepartmentRepository
+    {
+        Department GetDepartment(int id);
+
+        IEnumerable<Department> GetDepartments();
+
+        Department CreateDepartment(Department department);
+
+        Department EditDepartment(Department department,int id);
+
+        Department DeleteDepartment(int id);
+    }
+}
---
+++ WebApp/WebApp/Models/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+        Employee GetEmployee(int id);
+
+        IEnumerable<Employee> GetEmployees();
+
+        Employee CreateEmployee(Employee employee);
+
+        Employee EditEmployee(Employee employee,int id);
+
+        Employee DeleteEmployee(int id);
+    }
+}
