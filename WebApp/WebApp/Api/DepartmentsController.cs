8d10f25c12c7d245fdc189d76a37669a7c98a98b
Ahmet Durmic <ahmetdurmic97@outlook.com>
2020-03-18 17:30:46 +0100

    Implemented RESTful web api

---
+++ WebApp/WebApp/Api/DepartmentsController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models.Interfaces;
+using WebApp.Models;
+using WebApp.Dtos;
+namespace WebApp.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentsController : ControllerBase
+    {
+        private IDepartmentRepository _departmentRepository;
+        private IMapper _mapper;
+
+        public DepartmentsController(IDepartmentRepository departmentRepository, IMapper mapper)
+        {
+            _departmentRepository = departmentRepository;
+            _mapper = mapper;
+        }
+
+        //GET /api/departments
+        [HttpGet]
+        public IActionResult GetDepartments()
+        {
+            var departmentDtos = _departmentRepository.GetDepartments()
+                .Select(_mapper.Map<Department, DepartmentDto>);
+            return Ok(departmentDtos);
+        }
+
+        //GET /api/departments/1
+        [HttpGet("{id}")]
+        public IActionResult GetDepartment(int id)
+        {
+            Department departmentInDb = _departmentRepository.GetDepartment(id);
+
+            if (departmentInDb == null)
+                return NotFound();
+
+            return Ok(_mapper.Map<Department, DepartmentDto>(departmentInDb));
+        }
+
+        //CREATE /api/departments
+        [HttpPost]
+        public IActionResult CreateDepartment(DepartmentDto departmentDto)
+        {
+            ModelState.Remove("departmentDto.Id");
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            Department createdDepartment = _departmentRepository.
+                CreateDepartment(_mapper.Map<DepartmentDto, Department>(departmentDto));
+
+            departmentDto.Id = createdDepartment.Id;
+
+            return CreatedAtAction(nameof(departmentDto), new { id = departmentDto.Id }, departmentDto);
+        }
+
+        //PUT /api/departments/1
+        [HttpPut("{id}")]
+        public IActionResult EditDepartment(DepartmentDto departmentDto, int id)
+        {
+            ModelState.Remove("departmentDto.Id");
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            Department departmentInDb = _departmentRepository.GetDepartment(id);
+            if (departmentInDb == null)
+                return NotFound();
+
+            _mapper.Map(departmentDto, departmentInDb);
+            departmentDto = _mapper.Map<Department, DepartmentDto>(
+                _departmentRepository.EditDepartment(departmentInDb, departmentInDb.Id)
+                );
+
+            return Ok(departmentDto);
+        }
+
+        //DELETE /api/departments/1
+        [HttpDelete("{id}")]
+        public IActionResult DeleteDepartment(int id)
+        {
+            Department departmentInDb = _departmentRepository.GetDepartment(id);
+            if (departmentInDb == null)
+                return NotFound();
+            
+            return Ok(_mapper.Map<Department, DepartmentDto>(_departmentRepository.DeleteDepartment(id)));
+        }
+    }
+}
---
+++ WebApp/WebApp/Api/EmployeesController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.DbContext;
+using WebApp.Models.Interfaces;
+using WebApp.Dtos;
+using WebApp.Models;
+
+namespace WebApp.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private IEmployeeRepository _employeeRepository;
+        private IMapper _mapper;
+
+        public EmployeesController(IEmployeeRepository employeeRepository, IMapper mapper)
+        {
+            _employeeRepository = employeeRepository;
+            _mapper = mapper;
+        }
+
+        //GET /api/employees
+        [HttpGet]
+        public IActionResult GetEmployees()
+        {
+            var employeeDtos = _employeeRepository.GetEmployees()
+                .Select(_mapper.Map<Employee, EmployeeDto>);
+            return Ok(employeeDtos);
+        }
+
+        //GET /api/employees/1
+        [HttpGet("{id}")]
+        public IActionResult GetEmployee(int id)
+        {
+            Employee employeeInDb = _employeeRepository.GetEmployee(id);
+
+            if (employeeInDb == null)
+                return NotFound();
+
+            return Ok(_mapper.Map<Employee, EmployeeDto>(employeeInDb));
+        }
+
+        //CREATE /api/employees
+        [HttpPost]
+        public IActionResult CreateEmployee(EmployeeDto employeeDto)
+        {
+            ModelState.Remove("employeeDto.Id");
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            Employee createdEmployee = _employeeRepository.
+                CreateEmployee(_mapper.Map<EmployeeDto, Employee>(employeeDto));
+
+            employeeDto.Id = createdEmployee.Id;
+
+            return CreatedAtAction(nameof(employeeDto), new { id = employeeDto.Id }, employeeDto);
+        }
+
+        //PUT /api/employees/1
+        [HttpPut("{id}")]
+        public IActionResult EditEmployee(EmployeeDto employeeDto,int id)
+        {
+            ModelState.Remove("employeeDto.Id");
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            Employee empInDb = _employeeRepository.GetEmployee(id);
+            if (empInDb == null)
+                return NotFound();
+
+            _mapper.Map(employeeDto, empInDb);
+            employeeDto=_mapper.Map<Employee,EmployeeDto>(
+                _employeeRepository.EditEmployee(empInDb,empInDb.Id)
+                );
+
+            return Ok(employeeDto);
+        }
+
+        //DELETE /api/employees/1
+        [HttpDelete]
+        public IActionResult DeleteEmployee(int id)
+        {
+            Employee empInDb = _employeeRepository.GetEmployee(id);
+            if (empInDb == null)
+                return NotFound();
+
+            return Ok(_mapper.Map<Employee, EmployeeDto>(_employeeRepository.DeleteEmployee(id)));
+        }
+    }
+}
