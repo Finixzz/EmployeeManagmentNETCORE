808c0275f68e652277b219ed44f755b9c313325a
Ahmet Durmic <ahmetdurmic97@outlook.com>
2020-03-18 19:34:03 +0100

    Edited css, added datatables and department controller

---
+++ WebApp/WebApp/Controllers/DepartmentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models.Interfaces;
+
+namespace WebApp.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private IDepartmentRepository _departmentRepository;
+
+        public DepartmentController(IDepartmentRepository departmentRepository)
+        {
+            _departmentRepository = departmentRepository;
+        }
+        public IActionResult Index()
+        {
+            var departmentsInDb = _departmentRepository.GetDepartments().ToList();
+            return View(departmentsInDb);
+        }
+    }
+}
