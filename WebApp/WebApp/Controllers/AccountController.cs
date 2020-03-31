57b14fb9aa5755ccf0768c86f3d132e5f6b48c07
Ahmet Durmic <ahmetdurmic97@outlook.com>
2020-03-31 22:23:14 +0200

    Implemented login feature

--- WebApp/WebApp/Controllers/AccountController.cs
+++ WebApp/WebApp/Controllers/AccountController.cs
@@ -19,10 +19,64 @@ namespace WebApp.Controllers
             this.userManager = userManager;
             this.signInManager = signInManager;
         }
+
         [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                };
+                var result = await userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("index", "home");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                var result = await signInManager.PasswordSignInAsync(model.Email, model.Password,
+                                    model.RememberMe,false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("index", "home");
+                }
+    
+                ModelState.AddModelError(string.Empty,"Invalid Login Attempt");
+               
+            }
+            return View(model);
         }
 
         [HttpPost]
@@ -33,31 +87,6 @@ namespace WebApp.Controllers
         }
 
 
-        [HttpPost]
-        public async Task <IActionResult> Register(RegisterViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                var user = new IdentityUser
-                {
-                    UserName = model.Email,
-                    Email = model.Email,
-                };
-                var result=await userManager.CreateAsync(user,model.Password);
-
-                if (result.Succeeded)
-                {
-                    await signInManager.SignInAsync(user, isPersistent: false);
-                    return RedirectToAction("index", "home");
-                }
-
-                foreach(var error in result.Errors)
-                {
-                    ModelState.AddModelError("", error.Description);
-                }
-                
-            }
-            return View(model);
-        }
+       
     }
 }
