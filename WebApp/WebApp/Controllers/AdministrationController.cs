08a68918cccedf70c31bcf28f49b470996f591c2
Ahmet Durmic <ahmetdurmic97@outlook.com>
2020-04-06 16:33:30 +0200

    Implemented edit role feature

--- WebApp/WebApp/Controllers/AdministrationController.cs
+++ WebApp/WebApp/Controllers/AdministrationController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
 using WebApp.ViewModels;
 
 namespace WebApp.Controllers
@@ -11,10 +13,13 @@ namespace WebApp.Controllers
     public class AdministrationController : Controller
     {
         private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<ApplicationUser> userManager;
 
-        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        public AdministrationController(RoleManager<IdentityRole> roleManager,
+                                        UserManager<ApplicationUser> userManager)
         {
             this.roleManager = roleManager;
+            this.userManager = userManager;
         }
 
         [HttpGet]
@@ -43,7 +48,7 @@ namespace WebApp.Controllers
 
                 if (result.Succeeded)
                 {
-                    return RedirectToAction("index", "home");
+                    return RedirectToAction("index", "administration");
                 }
 
                 foreach (IdentityError error in result.Errors)
@@ -54,6 +59,61 @@ namespace WebApp.Controllers
             return View(model);
         }
 
-        //Edit and delete role actions TODO!!!!
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+            var role = await roleManager.FindByIdAsync(id);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with id={id} cannot be found";
+                return View("404");
+            }
+
+            var model = new EditRoleViewModel
+            {
+                Id = role.Id,
+                RoleName = role.Name
+            };
+            foreach (var user in userManager.Users.ToList())
+            {
+                if (await userManager.IsInRoleAsync(user, role.Name))
+                {
+                    model.Users.Add(user.UserName);
+                }
+            }
+            return View(model);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleViewModel model)
+        {
+            var role = await roleManager.FindByIdAsync(model.Id);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with id={model.Id} cannot be found";
+                return View("404");
+            }
+            else
+            {
+                role.Name = model.RoleName;
+                var result=await roleManager.UpdateAsync(role);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("index");
+                }
+
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View(model);
+
+            }
+        }
+
     }
 }
---
+++ WebApp/WebApp/ViewModels/EditRoleViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.ViewModels
+{
+    public class EditRoleViewModel
+    {
+        public string Id { get; set; }
+
+        [Required(ErrorMessage ="Role name is required")]
+        [Display(Name =" Role name")]
+        public string RoleName { get; set; }
+
+        public List<string> Users { get; set; }
+
+
+        public EditRoleViewModel()
+        {
+            Users = new List<string>();
+        }
+    }
+}
